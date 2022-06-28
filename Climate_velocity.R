bafe2b87e80654630b3a9e0650122b917f4943dd
lagruenburg <lkg2133@columbia.edu>
2022-06-28 11:44:32 -0400

    Adding GLorys climate vel

--- Climate_velocity.R
+++ Climate_velocity.R
@@ -1,14 +1,24 @@
 #Purpose: Vertical Climate Velocity in the NYB using ROMS data
 #Data: DOPPIO reanalysis for MidAtlantic Bight and Gulf of Maine (https://tds.marine.rutgers.edu/thredds/roms/espresso/catalog.html)
-# Last edited by L. Gruenburg July 9, 2021
+# Last edited by L. Gruenburg July 21, 2021
 library(ncdf4)
+library(VoCC)
+library(raster)
+citation("VoCC")
 
 #######Load the datasets
-ROMS_filepath = ('/Users/nyelab/Desktop/ClimateVelocity_Vertical/roms_converted.nc')
-ROMS_output <- nc_open(ROMS_filepath)
+#ROMS_filepath = ('/Users/nyelab/Desktop/ClimateVelocity_Vertical/roms_converted_temp10m.nc')
+ncpath <- "/Users/nyelab/Desktop/ClimateVelocity_Vertical/"
+ncname <- "roms_slice"  
+ncfname <- paste(ncpath, ncname, ".nc", sep="")#, stopIfNotEqualSpaced = F)
+dname <- "temp"
+tmp_raster <- brick(ncfname, varname="temp")
+
+#ROMS_output <- nc_open(ROMS_filepath)
 
 lon <- ncvar_get(ROMS_output, varid = "lon_rho")
 lat <- ncvar_get(ROMS_output, varid = "lat_rho")
-temp <- ncvar_get(ROMS_output, varid = 'temp')
-depth <- ncvar_get(ROMS_output, varid = "z_rho")
-time <-ncvar_get(ROMS_output,varid = 'new_time')
+temp <- ncvar_get(ROMS_output, varid = 'temp_10m')
+depth <- ncvar_get(ROMS_output, varid = "depth")
+time <-ncvar_get(ROMS_output,varid = 'ocean_time')
+
